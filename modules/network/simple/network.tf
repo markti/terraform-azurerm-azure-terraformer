f3f2f574626ed0260beb6028aef5bf854e1305ac
markti <markti@outlook.com>
2024-04-21 13:44:21 -0400

    add subnet space dynamically

--- modules/network/simple/network.tf
+++ modules/network/simple/network.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "main" {
   name                = "vnet-${var.name}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = [var.address_space]
 
 }
 
@@ -11,14 +11,14 @@ resource "azurerm_subnet" "bastion" {
   name                 = "AzureBastionSubnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = [cidrsubnet(var.address_space, 2, 0)]
 }
 
 resource "azurerm_subnet" "default" {
   name                 = "snet-default"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = [cidrsubnet(var.address_space, 2, 1)]
 }
 
 resource "azurerm_network_security_group" "default" {
